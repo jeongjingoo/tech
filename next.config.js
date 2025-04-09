4d28147820f323babc614deb685dd4918a1fc0f1
Jeongjingoo <godjava9@gmail.com>
2025-04-09 16:38:49 +0900

    Update: Next.js and related dependencies to latest version

--- next.config.js
+++ next.config.js
@@ -3,28 +3,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  images: {
-    unoptimized: true,
-    domains: ['localhost', 'dapi.kakao.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://dapi.kakao.com' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
-};
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig 
