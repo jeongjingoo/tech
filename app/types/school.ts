31dc0efacb2bdcd83b5845c2d16db0b6ae6d6ac3
Jeongjingoo <godjava9@gmail.com>
2025-04-10 16:56:53 +0900

    feat: 카카오맵 컴포넌트 및 타입 정의 추가

--- app/types/school.ts
+++ app/types/school.ts
@@ -1,16 +1,14 @@
+export interface SchoolData {
+  name: string;
+  address: string;
+  teachers_room_num: string;
+  admin_room_num: string;
+  team: string;
+  lat: number;
+  lon: number;
+}
+
 export interface School {
-  _id: string;
-  data: {
-    name: string;
-    istech: number;
-    address: string;
-    total_classes: number;
-    teachers_room_num: string;
-    admin_room_num: string;
-    team: string;
-    lat: number;
-    lon: number;
-  };
-  iscomp: number;
-  createdAt: string;
+  id: string;
+  data: SchoolData;
 }
