a6510cfd8fdd87f223e4870a94b95d25091197a4
Jeongjingoo <godjava9@gmail.com>
2025-04-10 16:48:51 +0900

    feat: School 타입 정의 추가

---
+++ tech/app/types/school.ts
@@ -0,0 +1,14 @@
+export interface SchoolData {
+  name: string;
+  address: string;
+  teachers_room_num: string;
+  admin_room_num: string;
+  team: string;
+  lat: number;
+  lon: number;
+}
+
+export interface School {
+  id: string;
+  data: SchoolData;
+} 
