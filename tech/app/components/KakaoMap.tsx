7fc939459d9e36618ca4146cdb7c13b1a5f218fd
Jeongjingoo <godjava9@gmail.com>
2025-04-10 16:50:27 +0900

    fix: TypeScript 설정 추가 및 import 경로 수정

--- tech/app/components/KakaoMap.tsx
+++ tech/app/components/KakaoMap.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { School } from '../../types/school';
+import { School } from '@/types/school';
 
 // @ts-ignore
 declare const kakao: any;
